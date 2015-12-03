780ee0acb4e312587bcb456fd2dd8cb54ac86815
Enzo Huaricapcha <lhuaricapcha@hotmail.com>
2015-12-03 14:25:53 -0500

    Roles de usuarios y autentificacion para ver empresas

--- middleware.js
+++ middleware.js
@@ -51,6 +51,33 @@ exports.ensureSuperAuthorized = function(req, res, next) {
   }
 }
 
+exports.ensureResponsableAuthorized = function(req, res, next) {
+  var bearerToken;
+  var bearerHeader = req.headers["authorization"];
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  }
+  else {
+    if (typeof bearerHeader !== 'undefined') {
+      var bearer = bearerHeader.split(" ");
+      bearerToken = bearer[1];
+      req.token = bearerToken;
+      User.findOne({where: {token: req.token}, attributes: ['uid', 'estado', 'rolRolId']})
+      .then(function(user) {
+        if(user == null) return res.sendStatus(403);
+        if (user.rolRolId === '104') {
+          req.user = user;
+          next();
+        } else {
+          res.sendStatus(401);
+        }
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  }
+}
+
 //funcion para agregar datos al header
 exports.allowCrossDomain = function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
