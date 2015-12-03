48aee78683e7e76c3f926b2517c72e87a324aedb
wjesus88 <wjesus88@gmail.com>
2015-12-03 09:27:06 -0500

    *

---
+++ scripts/cargarInspeccion.js
@@ -0,0 +1,28 @@
+var client = require('../dbConnection').getInstance().getClient();
+var Inspeccion = require('../models/Inspeccion');
+
+client.sync().then(function() {
+
+  var Inspecciones = [
+    {     
+      comentario:'xxx',
+    }  
+  ];
+
+  Inspecciones.forEach(function(item) {
+    Inspeccion.create({
+      comentario: item.comentario,
+     
+    })
+    .then(function(inspeccion) {
+      //console.log('Inspeccion ' + item.nombre + ' creado');
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+  })
+
+})
+.catch(function(err) {
+  console.log(err);
+});
