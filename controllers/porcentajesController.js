23e65ade8f61cf383fa315304b3a106bd0dda75b
Enzo Huaricapcha <lhuaricapcha@hotmail.com>
2015-12-09 16:22:39 -0500

    Lista los porcentajes

--- controllers/porcentajesController.js
+++ controllers/porcentajesController.js
@@ -1,33 +1,8 @@
 var Porcentaje = require('../models/Porcentaje');
 
 exports.index = function(req, res, next) {
-  var roca = req.query.roca;
-  var por_alto = req.query.porAlto;
-  var por_ancho = req.query.porAncho;
-  var por = 0;
-  var respuesta = {};
-  if (parseFloat(por_alto) > parseFloat(por_ancho)) {
-    por = por_alto;
-  } else {
-    por = por_ancho;
-  }
-  console.log(por);
-  Porcentaje.findAll({where: {rocaRocaid: roca}, order: 'porcentaje'}).then(function(porcentajes) {
-    if (porcentajes) {
-      var cont = 0;
-      porcentajes.forEach(function(item) {
-        if (parseFloat(item.porcentaje) >= por && cont === 0) {
-          respuesta = item;
-          cont = 1;
-        }
-      });
-      if (cont === 0) {
-        respuesta = porcentajes.pop();
-      }
-    }
-    var array = [];
-    array.push(respuesta);
-    res.status(200).jsonp(array);
+  Porcentaje.findAll({order: ['rocaRocaid', 'porcentaje']}).then(function(porcentajes) {
+    res.status(200).jsonp(porcentajes);
   })
   .catch(function(err) {
     res.status(500).send(err);
