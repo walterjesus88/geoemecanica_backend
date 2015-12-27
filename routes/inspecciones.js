90e49a892e5b136962d5129ea4cb77a64be5beea
Enzo Huaricapcha <lhuaricapcha@hotmail.com>
2015-12-26 21:00:16 -0500

    GET de inspecciones por id

--- routes/inspecciones.js
+++ routes/inspecciones.js
@@ -3,5 +3,6 @@ var routes = express.Router();
 var inspeccionesCtrl = require('../controllers/inspeccionesController');
 
 routes.get('/', inspeccionesCtrl.index);
+routes.get('/:id', inspeccionesCtrl.show);
 routes.post('/', inspeccionesCtrl.store);
 module.exports = routes;
